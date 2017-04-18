b3ed2f1e886cb20b1ceefa7e88e566ae7c807c31
csy <csy@csydeMac.local>
2017-04-18 13:10:41 +0800

    Initial Commit

---
+++ JYIndoorNavigation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYIndoorNavigation
+//
+//  Created by csy on 2017/4/18.
+//  Copyright © 2017年 chen_sy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYIndoorNavigation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JYIndoorNavigation
+//
+//  Created by csy on 2017/4/18.
+//  Copyright © 2017年 chen_sy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
